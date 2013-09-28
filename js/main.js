07cd9e41514135a00132fbb53f6513b661d40ae7
Coleman Foley <colemanfoley@gmail.com>
2013-09-28 14:35:59 -0700

    Added Rdio Utils.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+if (!rdioUtils.startupChecks()) {
+  return;
+}
+
 R.ready(function() {
 	console.log("R.ready called");
   R.player.play({source: "a3032151"}); // Alice In Chains - The Devil Put Dinosaurs Here
